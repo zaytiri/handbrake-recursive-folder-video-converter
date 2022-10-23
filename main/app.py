a192d7246f09ae1e295499ed7220038a8690518d
zay <64491586+zaytiri@users.noreply.github.com>
2022-10-23 15:29:10 +0100

    include args and multiple extensions
    app.py now includes the arguments given from the command; includes printing of multiple extensions in the intro text;

--- main/app.py
+++ main/app.py
@@ -3,32 +3,35 @@ import sys
 from configure_arguments import ConfigureArguments
 from search import Search
 
-root_path = 'C:\\Users\\ziia\\Desktop' # location of the following program: HandBrakeCLI.exe
-# folder_path = 'C:\\Users\\ziia\\Desktop\\advanced-python'
-
 # .\HandBrakeCLI.exe --preset "Very Fast 1080p30" -i
 # .\advanced-python\01-Introduction\01-Welcome.mp4 -o
 # .\advanced-python\01-Introduction\01-Welcome.mkv
 
 arguments = ConfigureArguments().configure_arguments()
 
-try:
-    folder_path = sys.argv[1]
-    original_file_extension = '.' + sys.argv[2]
-    target_file_extension = '.' + sys.argv[3]
-    to_delete_folder_name = 'TO-DELETE'
+folder_path = arguments.convert.value
+original_file_extension = arguments.extensions.value
+target_file_extension = arguments.target.value
+to_delete_folder_name = arguments.delete_folder.value
 
-    response = input(sys.argv[1] + ' is going to be modified permanently. Are you sure you want to continue? \n [Y/N]')
+response = input('"' + folder_path + '" is going to be modified permanently. Are you sure you want to continue? \n [Y/N]')
 
-    if response == 'N':
-        sys.exit()
-    elif response == 'Y':
-        print('All files found with the a ' + original_file_extension + ' extension are going to be converted to ' + target_file_extension +
-              ' extension and be kept in the "TO-DELETE" folder\n')
-
-        videos = Search(arguments.root.value, arguments.convert.value, arguments.extensions.value, arguments.target.value, arguments.delete_folder.value)
-        videos.search()
-
-except IndexError:
-    print('missing arguments')
+if response == 'N':
     sys.exit()
+elif response == 'Y':
+    print('All files found with the a ', end="")
+
+    first = True
+    for extension in original_file_extension:
+        if first:
+            print(extension, end="")
+            first = False
+            continue
+
+        print(' or ' + extension, end="")
+
+    print(' extension are going to be converted to ' + target_file_extension + ' extension and be kept in the "' + to_delete_folder_name + '" '
+                                                                                                                                           'folder.\n')
+
+    videos = Search(arguments.root.value, arguments.convert.value, arguments.extensions.value, arguments.target.value, arguments.delete_folder.value)
+    videos.search()

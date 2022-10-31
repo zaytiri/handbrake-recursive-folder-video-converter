3e57de9b07ec98b0a72cdc4228a322d6af23b773
zay <64491586+zaytiri@users.noreply.github.com>
2022-10-31 22:25:01 +0000

    changed visibility of root variable in directory

--- main/services/directory.py
+++ main/services/directory.py
@@ -7,18 +7,18 @@ class Directory:
     last_folder_path = ''
 
     def __init__(self, root):
-        self.__root = root
+        self.root = root
         self.get_current_folder()
         self.get_last_directory()
 
     def get_last_directory(self):
-        self.last_folder_path = os.path.dirname(self.__root)
+        self.last_folder_path = os.path.dirname(self.root)
 
     def get_current_folder(self):
-        self.current_folder = os.path.basename(os.path.normpath(self.__root))
+        self.current_folder = os.path.basename(os.path.normpath(self.root))
 
     def create(self, new_folder):
-        return os.path.join(self.__root, new_folder)
+        return os.path.join(self.root, new_folder)
 
     def create_folder(self, new_folder):
         new_directory = self.create(new_folder)
@@ -28,10 +28,10 @@ class Directory:
         return new_directory
 
     def search_through(self):
-        return os.walk(self.__root)
+        return os.walk(self.root)
 
     def remove(self):
-        os.remove(self.__root)
+        os.remove(self.root)
 
     def exists(self):
-        return exists(self.__root)
+        return exists(self.root)

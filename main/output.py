722901299f64747c1e299ac15c57e98d8c5161ed
zay <64491586+zaytiri@users.noreply.github.com>
2022-11-01 11:03:27 +0000

    updated output file
    added new information about files not encoded; added date when add the entries in the file;

--- main/output.py
+++ main/output.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from main.services.file import File
 
 
@@ -8,13 +10,20 @@ class Output:
     original_files_size = 0.0
     reduced_files_size = 0.0
     size_with_label = {}
+    files_not_encoded = []
 
     def __init__(self, absolute_path_parent):
         self.file = File(absolute_path_parent + '\\output.txt')
         self.file.open('a')
 
-    def add_file_information(self, video_file):
-        self.__add_line('The following video file is already converted: ' + video_file.name_only)
+    def add_file_information(self, video_file, successful):
+        self.__add_line('[' + str(datetime.now()) + ']\nThe following video file was converted: ' + video_file.name_only)
+
+        if not successful:
+            self.__add_line('\t*** This file was not encoded successfully! ***\n')
+            self.files_not_encoded.append(video_file.absolute_path + video_file.extension)
+            return
+
         self.__add_line('\t-Converted from ' + video_file.extension + ' extension to ' + video_file.target_extension)
         self.__set_message_with_size('\t-The original file size was: ', video_file.original_size)
         self.__set_message_with_size('\t-The converted file size is: ', video_file.converted_size)
@@ -25,13 +34,16 @@ class Output:
         self.reduced_files_size += video_file.converted_size
 
     def add_final_output(self):
-        self.__add_line('\nFinal Statistics:')
+        self.__add_line('[' + str(datetime.now()) + ']\nFinal Statistics:')
         self.__set_message_with_size('\tOriginal size of all searched video files: ', self.original_files_size)
         self.__set_message_with_size('\tReduced size of all converted video files: ', self.reduced_files_size)
 
         space_saved = self.original_files_size - self.reduced_files_size
         self.__set_message_with_size('\tSpace in disk saved: ', space_saved)
 
+        self.__add_line('\n\tThe following video files were not encoded successfully:')
+        for files in self.files_not_encoded:
+            self.__add_line('\t--> ' + files)
         self.file.close()
 
     def __set_message_with_size(self, message, size):

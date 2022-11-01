34452a66c73447f05f8516ef17a52d421564e51f
zay <64491586+zaytiri@users.noreply.github.com>
2022-11-01 09:18:39 +0000

    updated output class methods

--- main/output.py
+++ main/output.py
@@ -7,66 +7,53 @@ class Output:
     """
     original_files_size = 0.0
     reduced_files_size = 0.0
+    size_with_label = {}
 
     def __init__(self, absolute_path_parent):
         self.file = File(absolute_path_parent + '\\output.txt')
         self.file.open('a')
 
     def add_file_information(self, video_file):
-        self.add_line('The following video file is converted: ' + video_file.name_only)
-        self.add_line('\tThe absolute path is: ' + video_file.absolute_path)
-        self.add_line('\tThe original extension was: ' + video_file.extension)
-        self.add_line('\tThe target extension was: ' + video_file.target_extension)
-        self.add_line('\tThe original file size was: %.2f' % self.__convert_bytes(video_file.original_size) + self.__get_bytes_label(video_file.original_size))
-        self.add_line('\tThe converted file size is: %.2f' % self.__convert_bytes(video_file.converted_size) + self.__get_bytes_label(
-            video_file.converted_size))
-        self.add_line('\n\n')
+        self.__add_line('The following video file is already converted: ' + video_file.name_only)
+        self.__add_line('\t-Converted from ' + video_file.extension + ' extension to ' + video_file.target_extension)
+        self.__set_message_with_size('\t-The original file size was: ', video_file.original_size)
+        self.__set_message_with_size('\t-The converted file size is: ', video_file.converted_size)
+
+        self.__add_line('\n')
 
         self.original_files_size += video_file.original_size
         self.reduced_files_size += video_file.converted_size
 
     def add_final_output(self):
-        self.add_line('\n\nFinal Statistics:')
-        self.add_line('Original size of all searched video files: %.2f' % self.__convert_bytes(self.original_files_size) + self.__get_bytes_label(
-            self.original_files_size))
-        self.add_line('Reduced size of all converted video files: %.2f' % self.__convert_bytes(self.reduced_files_size) + self.__get_bytes_label(
-            self.reduced_files_size))
+        self.__add_line('\nFinal Statistics:')
+        self.__set_message_with_size('\tOriginal size of all searched video files: ', self.original_files_size)
+        self.__set_message_with_size('\tReduced size of all converted video files: ', self.reduced_files_size)
+
         self.file.close()
 
-    def add_line(self, message):
+    def __set_message_with_size(self, message, size):
+        self.__set_converted_bytes_with_label(size)
+        self.__add_line(message + '%.2f' % self.size_with_label['size'] + ' ' + self.size_with_label['label'])
+
+    def __add_line(self, message):
         self.file.write(message + '\n')
 
-    # todo: refactor the following static methods
-    @staticmethod
-    def __convert_bytes(size_in_bytes):
+    def __set_converted_bytes_with_label(self, size_in_bytes):
         kilobyte = 1024
         megabyte = kilobyte * kilobyte
         gigabyte = megabyte * kilobyte
 
         if kilobyte < size_in_bytes < megabyte:
             converted_size = size_in_bytes / kilobyte
+            label = 'KB'
         elif megabyte < size_in_bytes < gigabyte:
             converted_size = size_in_bytes / megabyte
+            label = 'MB'
         elif size_in_bytes >= gigabyte:
             converted_size = size_in_bytes / gigabyte
-        else:
-            converted_size = size_in_bytes
-
-        return converted_size
-
-    @staticmethod
-    def __get_bytes_label(size):
-        kilobyte = 1024
-        megabyte = kilobyte * kilobyte
-        gigabyte = megabyte * kilobyte
-
-        if kilobyte < size < megabyte:
-            label = 'KB'
-        elif megabyte < size < gigabyte:
-            label = 'MB'
-        elif size >= gigabyte:
             label = 'GB'
         else:
+            converted_size = size_in_bytes
             label = 'B'
 
-        return label
+        self.size_with_label = {'size': converted_size, 'label': label}

bddba18299e64e15b899ba016d1ba1449c024b0d
zay <64491586+zaytiri@users.noreply.github.com>
2022-11-09 19:22:59 +0000

    added message about 0 files found

--- havc/search.py
+++ havc/search.py
@@ -16,33 +16,40 @@ class Search:
     def search(self):
         main_directory = Directory(self.folder_path)
         output_file = Output(main_directory.root)
+        found_files = False
 
         for root, dirs, files in main_directory.search_through():
             if self.to_delete_folder_name in root:
                 continue
 
             for video in files:
+                found_files = True
                 current_video_file = VideoFile(video, self.folder_path, self.original_file_extensions, self.target_file_extension)
 
-                if current_video_file.process():
-                    handbrake = Command(self.root_path)
+                if not current_video_file.process():
+                    continue
 
-                    if self.custom_command.upper() != 'off'.upper():
-                        handbrake.set_custom_command(self.custom_command)
+                handbrake = Command(self.root_path)
 
-                    successful = handbrake.run_command(current_video_file)
+                if self.custom_command.upper() != 'off'.upper():
+                    handbrake.set_custom_command(self.custom_command)
 
-                    root_directory = Directory(root)
+                successful = handbrake.run_command(current_video_file)
 
-                    new_delete_directory = Directory(root_directory.last_folder_path)
-                    to_delete_folder_path = new_delete_directory.create_folder(self.to_delete_folder_name)
+                root_directory = Directory(root)
 
-                    new_deleted_folder_directory = Directory(to_delete_folder_path)
-                    new_deleted_folder_path = new_deleted_folder_directory.create_folder(root_directory.current_folder)
+                new_delete_directory = Directory(root_directory.last_folder_path)
+                to_delete_folder_path = new_delete_directory.create_folder(self.to_delete_folder_name)
 
-                    if successful:
-                        current_video_file.copy_to(new_deleted_folder_path)
+                new_deleted_folder_directory = Directory(to_delete_folder_path)
+                new_deleted_folder_path = new_deleted_folder_directory.create_folder(root_directory.current_folder)
 
-                    output_file.add_file_information(current_video_file, successful)
+                if successful:
+                    current_video_file.copy_to(new_deleted_folder_path)
+
+                output_file.add_file_information(current_video_file, successful)
+
+        if not found_files:
+            print('No files were found with current extensions.')
 
         output_file.add_final_output()

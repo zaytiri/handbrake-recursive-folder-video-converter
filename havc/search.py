1bf7fc67187e73322de1f3c3932d6f503cee4b1a
zay <64491586+zaytiri@users.noreply.github.com>
2023-02-28 17:55:44 +0000

    fixed to delete folder not being created correctly
    fixed to delete directory not creating same structure folder as the original

--- havc/search.py
+++ havc/search.py
@@ -21,6 +21,8 @@ class Search:
         output_file = Output(main_directory.root)
         found_files = False
 
+        new_main_delete_folder = self.create_main_delete_folder()
+
         for root, dirs, files in main_directory.search_through():
             if self.to_delete_folder_name in root:
                 continue
@@ -39,16 +41,10 @@ class Search:
 
                 successful = handbrake.run_command(current_video_file)
 
-                root_directory = Directory(root)
-
-                new_delete_directory = Directory(root_directory.last_folder_path)
-                to_delete_folder_path = new_delete_directory.create_folder(self.to_delete_folder_name)
-
-                new_deleted_folder_directory = Directory(to_delete_folder_path)
-                new_deleted_folder_path = new_deleted_folder_directory.create_folder(root_directory.current_folder)
+                sub_delete_folder = self.create_delete_folder_structure(root, new_main_delete_folder)
 
                 if successful:
-                    current_video_file.copy_to(new_deleted_folder_path)
+                    current_video_file.copy_to(sub_delete_folder)
 
                 output_file.add_file_information(current_video_file, successful)
 
@@ -61,3 +57,22 @@ class Search:
 
         if self.shutdown_when_done:
             subprocess.run(["shutdown", "-s"])
+
+    def create_main_delete_folder(self):
+        root_directory = Directory(self.folder_path)
+        new_delete_directory = Directory(root_directory.last_folder_path)
+        to_delete_folder_path = new_delete_directory.create_folder(self.to_delete_folder_name)
+        return Directory(to_delete_folder_path)
+
+    def create_delete_folder_structure(self, root, new_deleted_folder_directory):
+        root_directory = Directory(self.folder_path)
+        last_directory = Directory(root_directory.last_folder_path)
+        to_delete_folder = last_directory.root
+        path_name_list = root.split('\\')
+        for folder in path_name_list:
+            if folder == last_directory.current_folder:
+                to_delete_folder = to_delete_folder + '\\' + new_deleted_folder_directory.current_folder
+            if folder not in to_delete_folder:
+                to_delete_folder = to_delete_folder + '\\' + folder
+
+        return new_deleted_folder_directory.create_folder(to_delete_folder)

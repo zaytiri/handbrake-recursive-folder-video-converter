e8f814918a88ce18e5af54b72a8dfc0990269c7f
zay <64491586+zaytiri@users.noreply.github.com>
2023-03-03 18:42:49 +0000

    update impl of checking file's extension

--- havc/entities/video_file.py
+++ havc/entities/video_file.py
@@ -59,8 +59,9 @@ class VideoFile:
         self.name_only = '.'.join(file_name)
 
     def __process_extension_file(self):
+        file_name_extension = '.' + self.__name.split('.')[len(self.__name.split('.')) - 1]
         for ext in self.__file_extensions_to_convert:
-            if ext in self.__name:
+            if ext == file_name_extension:
                 self.extension = ext
                 return True
         return False
